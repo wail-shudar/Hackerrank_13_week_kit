e9e9d37a10f7f1af5d3debe39a11db777fbfad18
Wa'il Shudar <hello@shudar.com>
2022-05-24 13:01:45 -0400

    added week 8 problems

---
+++ 8_3_icecream_parlor/icecream_parlor.py
@@ -0,0 +1,40 @@
+#!/bin/python3
+# completed by: Wa'il Shudar
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'icecreamParlor' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+#  1. INTEGER m
+#  2. INTEGER_ARRAY arr
+#
+
+def icecreamParlor(m, arr):
+    for i, v in enumerate(arr, 1):
+        if m-v in set(arr[i:]): return [i, arr[i:].index(m-v)+i+1] 
+ 
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        m = int(input().strip())
+
+        n = int(input().strip())
+
+        arr = list(map(int, input().rstrip().split()))
+
+        result = icecreamParlor(m, arr)
+
+        fptr.write(' '.join(map(str, result)))
+        fptr.write('\n')
+
+    fptr.close()
---
+++ 8_4_inserting_a_node_into_a_doubly_sorted_linked_list/sorted_insert.py
@@ -0,0 +1,103 @@
+#!/bin/python3
+# completed by: Wail Shudar
+import math
+import os
+import random
+import re
+import sys
+
+class DoublyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+        self.prev = None
+
+class DoublyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insert_node(self, node_data):
+        node = DoublyLinkedListNode(node_data)
+
+        if not self.head:
+            self.head = node
+        else:
+            self.tail.next = node
+            node.prev = self.tail
+
+
+        self.tail = node
+
+def print_doubly_linked_list(node, sep, fptr):
+    while node:
+        fptr.write(str(node.data))
+
+        node = node.next
+
+        if node:
+            fptr.write(sep)
+
+#
+# Complete the 'sortedInsert' function below.
+#
+# The function is expected to return an INTEGER_DOUBLY_LINKED_LIST.
+# The function accepts following parameters:
+#  1. INTEGER_DOUBLY_LINKED_LIST llist
+#  2. INTEGER data
+#
+
+#
+# For your reference:
+#
+# DoublyLinkedListNode:
+#     int data
+#     DoublyLinkedListNode next
+#     DoublyLinkedListNode prev
+#
+#
+
+def sortedInsert(llist, data):
+    head = llist
+    node = DoublyLinkedListNode(data)
+    
+    while llist:
+        if llist.data >= data:        
+            node.next = llist
+            
+            if llist.prev is None: 
+                llist.prev = node
+                return node
+            else:               
+                node.prev = llist.prev
+                node.prev.next = llist.prev = node
+                return head
+                
+        elif llist.next is None:
+            node.prev = llist
+            llist.next = node
+            return head
+        llist = llist.next
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input())
+
+    for t_itr in range(t):
+        llist_count = int(input())
+
+        llist = DoublyLinkedList()
+
+        for _ in range(llist_count):
+            llist_item = int(input())
+            llist.insert_node(llist_item)
+
+        data = int(input())
+
+        llist1 = sortedInsert(llist.head, data)
+
+        print_doubly_linked_list(llist1, ' ', fptr)
+        fptr.write('\n')
+
+    fptr.close()
